d9ad73093fa96fe001402fd2f6f25fbc2ce01dc4
Tomas Thor Jonsson <benregn@gmail.com>
2011-08-17 08:15:33 +0200

    __name__ == __main__

--- scr/docstolatex.py
+++ scr/docstolatex.py
@@ -8,5 +8,12 @@ def main():
     username = raw_input('Enter your username: ')
     password = raw_input('Enter your password: ')
     client.client_login(username, password, client.source)
+    GetFolderList()
 
-    
+
+def GetFolderList():
+    feed = client.GetDocList(uri='/feeds/default/private/full/-/folder')
+
+
+if __name__ == '__main__':
+    main()
