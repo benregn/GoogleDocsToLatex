0a0ffce1a9bbc6d0af081565b3defe57a291ffce
Tomas Thor Jonsson <benregn@gmail.com>
2011-08-26 12:30:40 +0200

    started get_selected_folder; moved print_feed to the top

--- docstolatex/docstolatex.py
+++ docstolatex/docstolatex.py
@@ -5,6 +5,19 @@ from getpass import getpass
 client = client.DocsClient(source='benregn-GoogleDocsToLaTeX-v1')
 
 
+def print_feed(feed):
+    """Prints out the contents of a feed to the console."""
+    print '\n'
+    if not feed.entry:
+        print 'No entries in feed.\n'
+    print '%-30s %-20s %-12s %s' % ('TITLE', 'PARENT', 'TYPE', 'RESOURCE ID')
+    for entry in feed.entry:
+        print '%-30s %-20s %-12s %s' % (entry.title.text.encode('UTF-8'),
+                                        [f.title for f in entry.InFolders()],
+                                        entry.GetDocumentType(),
+                                        entry.resource_id.text)
+
+
 def get_folder_list():
     feed = client.GetDocList(uri='/feeds/default/private/full/-/folder')
     print_feed(feed)
@@ -18,18 +31,15 @@ def searching_title(query):
     print_feed(feed)
 
 
-def print_feed(feed):
-    """Prints out the contents of a feed to the console."""
-    print '\n'
-    if not feed.entry:
-        print 'No entries in feed.\n'
-    print '%-30s %-20s %-12s %s' % ('TITLE', 'PARENT', 'TYPE', 'RESOURCE ID')
-    for entry in feed.entry:
-        print '%-30s %-20s %-12s %s' % (entry.title.text.encode('UTF-8'),
-                                        [f.title for f in entry.InFolders()],
-                                        entry.GetDocumentType(),
-                                        entry.resource_id.text)
-    
+def get_selected_folder(folder_name):
+    feed = get_folder_list()
+    for folder in feed.entry:
+        if folder.title.text.encode('UTF-8') == folder_name:
+            print 'Contents of folder: ' + folder_name + '   ' + folder.content.src
+            folder_feed = client.GetDocList(uri=folder.content.src)
+            for doc in folder_feed.entry:
+                print doc.title.text, [f.title for f in doc.InFolders()]
+
 
 def main():
     username = raw_input('Enter your username: ')
@@ -37,6 +47,8 @@ def main():
     #password = getpass('Enter your password: ')
     client.client_login(username, password, client.source)
     get_folder_list()
+    folder_name = raw_input('Select folder: ')
+    get_selected_folder(folder_name)
     #searching_title('Level')
 
 
