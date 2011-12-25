51adefac83afb0be88b24049d29a6ef7ed7aa41c
Tomas Thor Jonsson <benregn@gmail.com>
2011-12-25 16:44:59 +0100

    config file name now passed in when instantiated

--- docstolatex/configparser.py
+++ docstolatex/configparser.py
@@ -1,20 +1,21 @@
 import ConfigParser
 
 class ConfigFileParser:
-    def __init__(self):
+    def __init__(self, filename):
         self.config = ConfigParser.SafeConfigParser(allow_no_value=True)
+        self.config_filename = filename
 
     def write_config_file(self):
         self.config.add_section('DocsToLatex')
         self.config.set('DocsToLatex', 'username', '')
         self.config.set('DocsToLatex', 'folder_name', '')
 
-        with open('config.cfg', 'wb') as configfile:
+        with open(self.config_filename, 'wb') as configfile:
             self.config.write(configfile)
 
 
     def read_config_file(self):
-        self.config.read('config.cfg')
+        self.config.read(self.config_filename)
 
         self.username = self.config.get('DocsToLatex', 'username')
         self.folder_name = self.config.get('DocsToLatex', 'folder_name')
